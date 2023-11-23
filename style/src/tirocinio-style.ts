95fb482c5672ee72b5274f225a65251e9619a7ac
damiano.finocchiaro <damiano.finocchiaro@cybersecurity.leonardocompany.com>
2023-11-23 10:46:20 +0100

    created utility styleGuide

---
+++ style/src/tirocinio-style.ts
@@ -0,0 +1,7 @@
+// Anything exported from this file is importable by other in-browser modules.
+
+import "popper.js/dist/umd/popper.js";
+import "bootstrap/scss/bootstrap.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+export function publicApiFunction() {}
---
+++ style/webpack.config.js
@@ -0,0 +1,43 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-ts");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "tirocinio",
+    projectName: "style",
+    webpackConfigEnv,
+    argv,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          include: [/node_modules/, /src/],
+          use: [
+          ],
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            "style-loader", // 3. Inject styles into DOM
+            "css-loader", // 2. Turns css into commonjs
+            "sass-loader", // 1. Turns sass into css
+          ],
+        },
+        {
+          test: /\.m?js/,
+          type: "javascript/auto",
+        },
+        {
+          test: /\.m?js/,
+          resolve: {
+            fullySpecified: false,
+          },
+        },
+      ],
+    },
+  });
+};
