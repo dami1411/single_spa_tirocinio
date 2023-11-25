3fb6f91c8e83e5ac5a92291d6eed061fbaa67fc1
damiano.finocchiaro <damiano.finocchiaro@cybersecurity.leonardocompany.com>
2023-11-25 13:26:56 +0100

    feat:add style scss file to share common theme

--- style/src/tirocinio-style.ts
+++ style/src/tirocinio-style.ts
@@ -3,5 +3,7 @@
 import "popper.js/dist/umd/popper.js";
 import "bootstrap/scss/bootstrap.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
+import "../assets/style.scss"
 
+  
 export function publicApiFunction() {}
